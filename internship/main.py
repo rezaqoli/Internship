d286afd7c7d2cfe261d86c46c8676817ce6693e1
[rezaqoli] <[gholipoor77@gmail.com]>
2020-07-15 09:19:13 +0430

    Dialog box

--- internship/main.py
+++ internship/main.py
@@ -1,20 +1,34 @@
 #!/usr/bin/python
 import sys
-from PySide2.QtWidgets import QApplication, QPushButton
-from PySide2.QtCore import Slot
+from PySide2.QtWidgets import (QLineEdit, QPushButton, QApplication,
+    QVBoxLayout, QDialog)
 
-# Greetings
-@Slot()
-def say_hello():
-    print("Button clicked, Hello!")
+class Form(QDialog):
 
-# Create the Qt Application
-app = QApplication(sys.argv)
-# Create a button
-button = QPushButton("Click me")
-# Connect the button to the function
-button.clicked.connect(say_hello)
-# Show the button
-button.show()
-# Run the main Qt loop
-app.exec_()
+    def __init__(self, parent=None):
+        super(Form, self).__init__(parent)
+        # Create widgets
+        self.edit = QLineEdit("Write my name here")
+        self.button = QPushButton("Show Greetings")
+        # Create layout and add widgets
+        layout = QVBoxLayout()
+        layout.addWidget(self.edit)
+        layout.addWidget(self.button)
+        # Set dialog layout
+        self.setLayout(layout)
+        # Add button signal to greetings slot
+        self.button.clicked.connect(self.greetings)
+
+    # Greets the user
+    def greetings(self):
+        print ("Hello %s" % self.edit.text())
+
+if __name__ == '__main__':
+    # Create the Qt Application
+    app = QApplication(sys.argv)
+    # Create and show the form
+    form = Form()
+    form.show()
+    # Run the main Qt loop
+    sys.exit(app.exec_())
+
